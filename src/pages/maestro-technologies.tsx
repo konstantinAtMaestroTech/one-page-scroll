027e5adc80a377be1d9899529f6ef88dd6729f63
konstantinAtMaestroTech <konstantin@maestro-tech.com>
2025-04-05 11:27:47 +0200

    mobile pages try

--- src/pages/maestro-technologies.tsx
+++ src/pages/maestro-technologies.tsx
@@ -2,12 +2,63 @@ import React from 'react';
 import { AnimatePresence } from 'framer-motion';
 import AnimatedSection from '../components/animated-section';
 import { useActivePage } from '../hooks/useActivePage';
+import { useIsMobile } from '../hooks/useIsMobile';
 
-interface AboutPageProps {
+interface MaestroBioProps {
   pageIndex: number;
 }
 
-const MaestroBio: React.FC<AboutPageProps> = ({ pageIndex }) => {
+const MaestroBio: React.FC<MaestroBioProps> = ({ pageIndex }) => {
+  const isMobile = useIsMobile();
+
+  return isMobile ? <MaestroBioMobile pageIndex={pageIndex} /> : <MaestroBioDesktop pageIndex={pageIndex} />
+}
+
+const MaestroBioMobile: React.FC<MaestroBioProps> = ({ pageIndex }) => {
+  const { isActive } = useActivePage({ pageIndex, delay: 300 });
+
+  return (
+    <div className="h-full w-full text-left text-white px-10">
+      <AnimatePresence>
+        {isActive && (
+          <>            
+            <div className="grid grid-rows-3 w-full h-full">
+                <AnimatedSection 
+                direction="right" 
+                delay={0.5}
+                className='mt-20'
+                >
+                    <div className="text-left text-5xl font-bold">
+                    <h3>
+                        Built by practitioners, for practitioners.
+                    </h3>
+                    </div>
+                </AnimatedSection>
+                
+              <AnimatedSection 
+                direction="left" 
+                delay={0.5}
+                className='row-span-2 flex justify-center items-center'
+              >
+                <div className="text-left h-fit text-xl font-semibold">
+                  <p className="mb-4">
+                    Originally developed as an in-house tool at <a href="https://maestro-tech.com" className=" hover:text-[#FF4300] underline">Maestro Technologies</a> — a general contractor — Maestro Pilot was born from real-world challenges in project delivery. 
+                  </p>
+                  <p>
+                    To realise the transformative potential of prefabrication design, production, and assembly activities must run in sync.
+                    MaestroPilot makes this happen with a data-driven workflow customized to your project.
+                  </p>
+                </div>
+              </AnimatedSection>
+            </div>
+          </>
+        )}
+      </AnimatePresence>
+    </div>
+  )
+
+}        
+const MaestroBioDesktop: React.FC<MaestroBioProps> = ({ pageIndex }) => {
   const { isActive } = useActivePage({ pageIndex, delay: 300 });
 
   return (
