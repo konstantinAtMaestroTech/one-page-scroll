4646f762b966396981ef0598da9bf79b9c11586c
konstantinAtMaestroTech <konstantin@maestro-tech.com>
2025-04-06 11:36:44 +0200

    users mobile

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -2,12 +2,60 @@ import React from 'react';
 import { AnimatePresence } from 'framer-motion';
 import AnimatedSection from '../components/animated-section';
 import { useActivePage } from '../hooks/useActivePage';
+import { useIsMobile } from '../hooks/useIsMobile';
 
-interface AboutPageProps {
+interface UsersPageProps {
   pageIndex: number;
 }
 
-const Users: React.FC<AboutPageProps> = ({ pageIndex }) => {
+const Users: React.FC<UsersPageProps> = ({pageIndex}) => {
+  const isMobile = useIsMobile();
+  return isMobile ? <UsersMobile pageIndex={pageIndex}/> : <UsersDesktop pageIndex={pageIndex}/>
+}
+
+const UsersMobile: React.FC<UsersPageProps> = ({pageIndex}) => {
+  const { isActive } = useActivePage({ pageIndex, delay: 300 });
+
+  return (
+    <div className="h-svh text-center text-white px-4 pt-18">
+      <AnimatePresence>
+        {isActive && (
+          <>            
+            <div className="flex flex-col px-6 relative">
+              <AnimatedSection 
+                direction="up" 
+                delay={0.5}
+                className='flex justify-end border-b-1 border-white/30'
+              >
+                <div className="flex flex-col text-left text-lg p-2 items-start">
+                  <p>
+                    For <span className='font-bold'>fabricators</span>, <span className='font-bold'>manufacturers</span> and <span className='font-bold'>installers</span>: optimise your operations 
+                  </p>
+                  <p>
+                    Upload your production models, plan logistics and assembly, and equip your crews with the real-time data they need to get the job done—faster and more accurately. 
+                  </p>
+                </div>
+              </AnimatedSection>
+                
+              <AnimatedSection direction="down" delay={0.5}>
+                <div className="flex flex-col text-left text-lg p-2 ">
+                  <p>
+                    For <span className="font-bold">General Contractors</span>, <span className="font-bold">Project Managers</span> and <span className='font-bold'>Developers</span>: master the complexity of your jobsite
+                  </p>
+                  <p>
+                    Specify Maestro Pilot in your tender and let software be your boots on the ground: track production progress, manage complex scope splits and ensure assembly goes to plan.
+                  </p>
+                </div>
+              </AnimatedSection>
+            </div>
+          </>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
+
+const UsersDesktop: React.FC<UsersPageProps> = ({ pageIndex }) => {
   const { isActive } = useActivePage({ pageIndex, delay: 300 });
 
   return (
