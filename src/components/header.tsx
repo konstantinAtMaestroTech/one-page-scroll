b8ffe4b360e9d798d3911424e0025ad8a7f7707b
konstantinAtMaestroTech <konstantin@maestro-tech.com>
2025-04-07 17:56:48 +0200

    header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import { useActivePage } from '../hooks/useActivePage';
 import AnimatedSection from './animated-section';
 import { MoveRight } from 'lucide-react';
+import { useIsMobile } from '../hooks/useIsMobile';
 
 interface HeaderProps {
   logoSrc?: string;
@@ -10,6 +11,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ logoSrc = "/logo.svg" }) => {
   const { activePage, navigateToPage } = useActivePage({ pageIndex: 0 });
+  const isMobile = useIsMobile();
   const isHomePage = activePage === 0;
   const isSpecialPage = activePage === 5;
   
@@ -89,15 +91,33 @@ const Header: React.FC<HeaderProps> = ({ logoSrc = "/logo.svg" }) => {
         <img 
           src={logoSrc} 
           alt="Company Logo" 
-          className="h-12 w-auto"
+          className="flex h-12 w-auto "
         />
       </motion.div>
       <AnimatedSection 
         delay={0.4} 
         direction="up"
+        className='flex gap-2'
       >
+        <motion.a
+          href="https://construction-data-hub.vercel.app"
+          className="flex w-full justify-between border-2 bg-transparent font-semibold py-1 md:py-2 px-3 md:px-6 rounded-full text-sm md:text-base"
+          initial="default"
+          animate={isSpecialPage ? "special" : "default"}
+          variants={buttonColorVariants}
+          whileHover={{ 
+            scale: 1.05, 
+            background: isSpecialPage ? "white" : "#FF4300", 
+            color: isSpecialPage ? "#FF4300" : "white",
+            borderColor: isSpecialPage ? "white" : "#FF4300"
+          }}
+          whileTap={{ scale: 0.95 }}
+          transition={{ type: "spring", stiffness: 400, damping: 17 }}
+        >
+          Log In
+        </motion.a>
         <motion.button 
-          className="flex w-full justify-between gap-4 border-2 bg-transparent font-semibold py-2 px-6 rounded-full"
+          className="flex w-full whitespace-nowrap justify-between gap-2 md:gap-4 border-2 bg-transparent font-semibold py-1 md:py-2 px-3 md:px-6 rounded-full text-sm md:text-base"
           initial="default"
           animate={isSpecialPage ? "special" : "default"}
           variants={buttonColorVariants}
@@ -112,7 +132,7 @@ const Header: React.FC<HeaderProps> = ({ logoSrc = "/logo.svg" }) => {
           onClick={() => navigateToPage(6)}
         >
           Get Started
-          <MoveRight/>
+          {!isMobile && <MoveRight size={18} />}
         </motion.button>
       </AnimatedSection>
     </motion.header>
